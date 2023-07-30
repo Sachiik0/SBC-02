848b7e3cbc1416d46e24738ce1b092a744573d9c
Sachiik0 <mmasanjuan@tip.edu.ph>
2023-07-31 00:06:00 +0800

    sa.ts

---
+++ sa.ts
@@ -0,0 +1,39 @@
+import base58 from 'bs58';
+import * as Web3 from '@solana/web3.js'
+import { lamports } from '@metaplex-foundation/js';
+import { SystemProgram, LAMPORTS_PER_SOL, sendAndConfirmTransaction } from '@solana/web3.js';
+
+
+
+async function main() {
+    const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+    const base58DecodedPK = base58.decode('3Ch2xLVU6uSphFzsSzzLFZmr1w37YdQyKm6ULskN5B4H7xxjnUdWQT8wc1rQciw2bMB8MZa7n2N64nmvEuuVv8ks');
+    const signer = Web3.Keypair.fromSecretKey(base58DecodedPK);
+    const transaction = new Web3.Transaction();
+    const instruction = new Web3.TransactionInstruction({
+        keys: [
+            {
+                pubkey: new Web3.PublicKey('uyh4oNM21BcinrKeAKmaeKAXcG4K2ro6bFFPpBPdCwm'),
+                isSigner: true,
+                isWritable: false,
+                
+            }
+        ],
+        data: Buffer.alloc(20),
+        programId: new Web3.PublicKey('uyh4oNM21BcinrKeAKmaeKAXcG4K2ro6bFFPpBPdCwm'),
+        
+    });
+    transaction.add(instruction);
+    const signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [signer],
+        
+    )
+    console.log('SIGNATURE', signature)
+}
+main()
+.then(() => process.exit(0))
+.catch(err => {
+    console.error(err)
+});
